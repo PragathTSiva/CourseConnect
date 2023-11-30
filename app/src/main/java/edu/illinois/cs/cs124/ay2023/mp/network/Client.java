a1b3cd049b0573470ea6c6ab54fa2836a221e87a
pragath <ptsiva2@illinois.edu>
2023-11-30 00:31:15 -0600

    MP3 Setup

--- app/src/main/java/edu/illinois/cs/cs124/ay2023/mp/network/Client.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2023/mp/network/Client.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import edu.illinois.cs.cs124.ay2023.mp.application.CourseableApplication;
 import edu.illinois.cs.cs124.ay2023.mp.helpers.ResultMightThrow;
 import edu.illinois.cs.cs124.ay2023.mp.models.Course;
+import edu.illinois.cs.cs124.ay2023.mp.models.Rating;
 import edu.illinois.cs.cs124.ay2023.mp.models.Summary;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -87,7 +88,11 @@ public final class Client {
     requestQueue.add(courseRequest);
   }
 
-  public void getRating(Summary summary, @NonNull Consumer<ResultMightThrow<Rating>> callback) {
+  public void getRating(@NonNull Summary summary, @NonNull Consumer<ResultMightThrow<Rating>> callback) {
+    callback.accept(new ResultMightThrow<>(new IllegalStateException()));
+  }
+
+  public void postRating(@NonNull Rating rating, @NonNull Consumer<ResultMightThrow<Rating>> callback) {
     callback.accept(new ResultMightThrow<>(new IllegalStateException()));
   }
   // You should not need to modify the code below
