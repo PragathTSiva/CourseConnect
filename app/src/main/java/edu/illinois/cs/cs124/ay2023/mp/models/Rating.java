73bb7092442c0fe18c44c733d2accc37e58b0b98
pragath <ptsiva2@illinois.edu>
2023-11-30 00:33:38 -0600

    MP3 Setup

--- app/src/main/java/edu/illinois/cs/cs124/ay2023/mp/models/Rating.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2023/mp/models/Rating.java
@@ -1,7 +1,7 @@
 package edu.illinois.cs.cs124.ay2023.mp.models;
 
 public class Rating {
-  private static final float NOT_RATED = -1.0f;
+  public static final float NOT_RATED = -1.0f;
   private float rating = NOT_RATED;
 
   private Summary summary;
